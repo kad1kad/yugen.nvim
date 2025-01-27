83fb95ab253ad8cd9279ae05434d3d3fca33cfc4
kad1kad <k.aliu@me.com>
2025-01-27 16:24:29 +0000

    Change bg color

--- lua/yugen/palette.lua
+++ lua/yugen/palette.lua
@@ -1,22 +1,21 @@
 local variants = {
-  main = {
-    placeholder = '#FFFF00',
-    color100 = '#FAFAFA',
-    color200 = '#D4D4D4',
-    color300 = '#A9A9A9',
-    color400 = '#696969',
-    color500 = '#505050',
-    color600 = '#303030',
-    color700 = '#151515',
-    color800 = '#1c1c1c',
-    primary = '#FFBE89',
-    success = '#7EAB8E',
-    warning = '#FFF2AF',
-    error = '#F57A7A',
-    none = 'none'
-  },
+	main = {
+		placeholder = "#FFFF00",
+		color100 = "#FAFAFA",
+		color200 = "#D4D4D4",
+		color300 = "#A9A9A9",
+		color400 = "#696969",
+		color500 = "#505050",
+		color600 = "#303030",
+		color700 = "#151515",
+		color800 = "#060606",
+		primary = "#FFBE89",
+		success = "#7EAB8E",
+		warning = "#FFF2AF",
+		error = "#F57A7A",
+		none = "none",
+	},
 }
-
 
 local palette = {}
 
